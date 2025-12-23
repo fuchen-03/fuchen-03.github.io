6852af52448a1402652540f728bf600cbe551bfb
fuchen-03 <1337220360@qq.com>
2025-12-23 20:00:38 +0800

    Site updated: 2025-12-23 20:00:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/22/VSCode远程连接docker/","2025/12/22/hexo使用入门/"];function toRandomPost(){
+var posts=["2025/12/22/hexo使用入门/","2025/12/22/VSCode远程连接docker/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
